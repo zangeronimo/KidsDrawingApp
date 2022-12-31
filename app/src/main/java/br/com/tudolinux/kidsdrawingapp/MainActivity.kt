a849a940c44af273bafcc91d6bbf123a5695735d
Zangeronimo <zangeronimo@gmail.com>
2022-12-31 16:58:04 -0300

    feat: add a share feature to shate the image

--- app/src/main/java/br/com/tudolinux/kidsdrawingapp/MainActivity.kt
+++ app/src/main/java/br/com/tudolinux/kidsdrawingapp/MainActivity.kt
@@ -8,6 +8,7 @@ import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
+import android.media.MediaScannerConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
@@ -219,6 +220,7 @@ class MainActivity : AppCompatActivity() {
                         cancelProgressDialog()
                         if(result.isNotEmpty()){
                             Toast.makeText(this@MainActivity, "File saved successfully : $result", Toast.LENGTH_SHORT).show()
+                            shareImage(result)
                         } else {
                             Toast.makeText(this@MainActivity, "Something went wrong while saving the file.", Toast.LENGTH_SHORT).show()
                         }
@@ -243,6 +245,17 @@ class MainActivity : AppCompatActivity() {
         if(customProgressDialog != null){
             customProgressDialog?.dismiss()
             customProgressDialog = null
+        }
+    }
+
+    private fun shareImage(result: String) {
+        MediaScannerConnection.scanFile(this, arrayOf(result), null){
+            path, uri ->
+            val shareIntent = Intent()
+            shareIntent.action = Intent.ACTION_SEND
+            shareIntent.putExtra(Intent.EXTRA_STREAM, uri)
+            shareIntent.type = "image/png"
+            startActivity(Intent.createChooser(shareIntent, "Share"))
         }
     }
 }
