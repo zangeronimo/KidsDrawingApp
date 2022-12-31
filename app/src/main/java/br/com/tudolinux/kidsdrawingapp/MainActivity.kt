254cbf09fabac9517c6e22a51c6f2e8c7ffd5a74
Zangeronimo <zangeronimo@gmail.com>
2022-12-31 10:17:29 -0300

    feat: select the brush size

--- app/src/main/java/br/com/tudolinux/kidsdrawingapp/MainActivity.kt
+++ app/src/main/java/br/com/tudolinux/kidsdrawingapp/MainActivity.kt
@@ -1,7 +1,10 @@
 package br.com.tudolinux.kidsdrawingapp
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,5 +16,35 @@ class MainActivity : AppCompatActivity() {
 
         drawingView = findViewById(R.id.drawing_view)
         drawingView?.setSizeForBrush(10.toFloat())
+
+        val ib_brush : ImageButton = findViewById(R.id.ib_brush)
+
+        ib_brush.setOnClickListener{
+            showBrushSizeChooserDialog()
+        }
+
+    }
+
+    private fun showBrushSizeChooserDialog(){
+        val brushDialog = Dialog(this)
+        brushDialog.setContentView(R.layout.dialog_brush_size)
+        brushDialog.setTitle("Brush size: ")
+        val smallBtn: ImageView = brushDialog.findViewById(R.id.ib_small_brush)
+        smallBtn.setOnClickListener {
+            drawingView?.setSizeForBrush(5.toFloat())
+            brushDialog.dismiss()
+        }
+        val mediumBtn: ImageView = brushDialog.findViewById(R.id.ib_medium_brush)
+        mediumBtn.setOnClickListener {
+            drawingView?.setSizeForBrush(10.toFloat())
+            brushDialog.dismiss()
+        }
+        val largeBtn: ImageView = brushDialog.findViewById(R.id.ib_large_brush)
+        largeBtn.setOnClickListener {
+            drawingView?.setSizeForBrush(15.toFloat())
+            brushDialog.dismiss()
+        }
+
+        brushDialog.show()
     }
 }
